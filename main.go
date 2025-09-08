e686d62d846c6bb020990abf9f58dc0788558569
NikitaPRV <nikitaprv@oxygen>
2025-09-08 21:09:58 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type project struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+}
+
+var projects = []project{
+	{ID: "1", Title: "Roma's IPO"},
+	{ID: "2", Title: "Microsoft shop"},
+	{ID: "3", Title: "Apple juice"},
+}
+
+func getProjects(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, projects)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/projects", getProjects)
+
+	router.Run("localhost:8080")
+}
